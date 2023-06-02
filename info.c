f47ac98bafb4ce270bbc8b3ea7804af884a2e2b7
Chia-I Wu <olvaffe@gmail.com>
2023-06-01 23:13:55 -0700

    info: print default surface attrs

--- info.c
+++ info.c
@@ -31,6 +31,71 @@ info_images(const struct va *va)
 }
 
 static void
+info_pair_default_surface(struct va *va, const struct va_pair *pair)
+{
+    VAConfigID config;
+    va->status = vaCreateConfig(va->display, pair->profile, pair->entrypoint, NULL, 0, &config);
+    va_check(va, "failed to create config");
+
+    unsigned int count;
+    va->status = vaQuerySurfaceAttributes(va->display, config, NULL, &count);
+    va_check(va, "failed to query surface attr count");
+
+    VASurfaceAttrib *attrs = malloc(sizeof(*attrs) * count);
+    if (!attrs)
+        va_die("failed to alloc surface attrs");
+    va->status = vaQuerySurfaceAttributes(va->display, config, attrs, &count);
+    if (va->status != VA_STATUS_SUCCESS)
+        count = 0;
+
+    vaDestroyConfig(va->display, config);
+
+    va_log("  default surface attrs:");
+    for (unsigned int i = 0; i < count; i++) {
+        const VASurfaceAttrib *attr = &attrs[i];
+        if (attr->flags == VA_SURFACE_ATTRIB_NOT_SUPPORTED)
+            continue;
+
+        const char *name;
+        switch (attr->type) {
+#define CASE(a)                                                                                  \
+    case VASurfaceAttrib##a:                                                                     \
+        name = #a;                                                                               \
+        break
+            CASE(PixelFormat);
+            CASE(MinWidth);
+            CASE(MaxWidth);
+            CASE(MinHeight);
+            CASE(MaxHeight);
+            CASE(MemoryType);
+            CASE(ExternalBufferDescriptor);
+#undef CASE
+        default:
+            name = "Unknown";
+            break;
+        }
+
+        va_log("    %s: flags 0x%x", name, attr->flags);
+        switch (attr->value.type) {
+        case VAGenericValueTypeInteger:
+            va_log("      %d", attr->value.value.i);
+            break;
+        case VAGenericValueTypeFloat:
+            va_log("      %f", attr->value.value.f);
+            break;
+        case VAGenericValueTypePointer:
+            va_log("      pointer");
+            break;
+        case VAGenericValueTypeFunc:
+            va_log("      function");
+            break;
+        }
+    }
+
+    free(attrs);
+}
+
+static void
 info_pair_attr(const struct va *va, const struct va_pair *pair, const VAConfigAttrib *attr)
 {
     if (attr->value == VA_ATTRIB_NOT_SUPPORTED)
@@ -62,7 +127,7 @@ info_pair_attr(const struct va *va, const struct va_pair *pair, const VAConfigAt
 }
 
 static void
-info_pairs(const struct va *va)
+info_pairs(struct va *va)
 {
     for (int i = 0; i < va->pair_count; i++) {
         const struct va_pair *pair = &va->pairs[i];
@@ -72,6 +137,8 @@ info_pairs(const struct va *va)
             const VAConfigAttrib *attr = &pair->attrs[j];
             info_pair_attr(va, pair, attr);
         }
+
+        info_pair_default_surface(va, pair);
     }
 }
 
@@ -80,7 +147,7 @@ info_display(const struct va *va)
 {
     va_log("version: %d.%d", va->major, va->minor);
     va_log("vendor: %s", va->vendor);
-    va_log("attrs:");
+    va_log("display attrs:");
 
     for (int i = 0; i < va->attr_count; i++) {
         const VADisplayAttribute *attr = &va->attrs[i];
